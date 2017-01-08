517b5006aa9cc6c0edddea0037fdf71b5b96f111
Martin Ansong <martin.ansong@gmail.com>
2017-01-08 15:35:11 +0000

    add function for addition, subtraction add function for scalar multiplication

--- vector-module/vector.py
+++ vector-module/vector.py
@@ -12,6 +12,17 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
+    def plus(self, v):
+        added_coordinates = [ x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(added_coordinates)
+
+    def minus(self, v):
+        minus_coordinates = [ x-y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(minus_coordinates)
+
+    def scalarmultiplication(self, c):
+        multiplied_coordinates = map(lambda x: x*c, self.coordinates)
+        return Vector(multiplied_coordinates)
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
