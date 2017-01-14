1d671f78c22fd10e4b91700ef569ae141c5182b3
Martin Ansong <martin.ansong@gmail.com>
2017-01-14 19:11:33 +0000

    adding angle function

--- vector-module/vector.py
+++ vector-module/vector.py
@@ -1,11 +1,14 @@
-from math import sqrt
+from math import sqrt, acos, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
 
 class Vector(object):
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -23,7 +26,7 @@ class Vector(object):
         return Vector(minus_coordinates)
 
     def scalarmultiplication(self, c):
-        multiplied_coordinates = map(lambda x: x*c, self.coordinates)
+        multiplied_coordinates = map(lambda x: x*Decimal(c), self.coordinates)
         return Vector(multiplied_coordinates)
 
     def magnitude(self):
@@ -33,14 +36,30 @@ class Vector(object):
     def normalise(self):
         try:
             magnitude = self.magnitude()
-            return self.scalarmultiplication(1/magnitude)
+            return self.scalarmultiplication(Decimal('1.0')/magnitude)
 
         except ZeroDivisionError:
             raise Exception('Cannot normalise the zero vector')
 
     def dotproduct(self, v):
-        dot_prod = sum([ x*y for x,y in zip(self.coordinates, v.coordinates)])
+        dot_prod = sum([x*y for x,y in zip(self.coordinates, v.coordinates)])
         return dot_prod
+
+    # v.w = |v||w|cosx, x = arccos(v.w/|v||w|)
+    def angle(self, v, deg=False):
+        try:
+            v1 = self.normalise()
+            v2 = v.normalise()
+            angle_rad = acos(v1.dotproduct(v2))
+
+            if deg:
+                angle_deg = angle_rad * (180. / pi)
+                return angle_deg
+            else:
+                return angle_rad
+
+        except Exception as e:
+            raise e
 
     def angle(self, v):
         self.dotproduct(v)/(self.magnitude() * v.magnitude())
@@ -50,3 +69,10 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+v = Vector([3.183, -7.627])
+w = Vector([-2.668, 5.319])
+
+print v.dotproduct(w)
+
+print v.angle(w)
